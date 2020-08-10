d8a2baba92c0990550f9e842624c101174693afe
unknown <nikofolli@gmail.com>
2020-08-10 16:35:43 -0300

    conflicto por comentario

--- WindowsFormsApp1/WindowsFormsApp1/frmLogin.cs
+++ WindowsFormsApp1/WindowsFormsApp1/frmLogin.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
-{ // comentario algo
+{ // comentario algo 2.0
     public partial class frmLogin : Form
     {
         string user = "admin";
